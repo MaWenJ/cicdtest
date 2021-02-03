b92f66569af5bfb4ec1ddd0ce03e2ef43d0ea1dc
mawj <1187730883@qq.com>
2021-02-03 14:13:58 +0800

    test

---
+++ image-springboot/src/main/java/com/mwjstudy/docker/imagespringboot/ImageSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mwjstudy.docker.imagespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageSpringbootApplication.class, args);
+    }
+
+}
